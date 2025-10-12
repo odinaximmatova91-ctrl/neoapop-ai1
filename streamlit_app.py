d9cb49aa3189af632d6f65d486ab40ead1008713
odinaximmatova91-ctrl <odinaximmatova91@gmail.com>
2025-10-12 11:40:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,4 @@ st.write("Salom! Bu sening birinchi AI + Drug Discovery dasturing!")
 target = st.text_input("Kasallik yoki target protein nomini kiriting:")
 if st.button("Molekulalar yaratish"):
     st.success(f"Demo: {target} uchun 1000 molekula yaratildi (test holat)")
-import streamlit as st
 
-st.title("NeoApop-AI 🚀")
-st.write("Salom! Bu sening birinchi AI + Drug Discovery dasturing!")
-
-target = st.text_input("Kasallik yoki target protein nomini kiriting:")
-if st.button("Molekulalar yaratish"):
-    st.success(f"Demo: {target} uchun 1000 molekula yaratildi (test holat)")
